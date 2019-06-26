d69b12140a5ab282c7da9d8e52896fd181d536a5
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-06-26 07:53:03 +0200

    Support no deploy env variable to only package the binary.

--- deploy-maven-central.sh
+++ deploy-maven-central.sh
@@ -17,11 +17,21 @@ SONAR_ORGANIZATION=$(echo ${SECRETS} | jq -r '.data.SONAR_ORGANIZATION')
 
 echo "Building $BRANCH branch"
 
-if [ "$BRANCH" = "develop" ]; then
-  openssl aes-256-cbc -pass pass:${OPENSSL_PWD} -in ../build-resources/private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s ../build-resources/settings.xml org.jacoco:jacoco-maven-plugin:prepare-agent clean deploy sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=${SONAR_ORGANIZATION} -Dsonar.login=${SONAR}
-elif [ "$BRANCH" = "master" ]; then
-  openssl aes-256-cbc -pass pass:${OPENSSL_PWD} -in ../build-resources/private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s ../build-resources/settings.xml clean deploy
-  mvn -V -s ../build-resources/settings.xml deploy -Dmaven.plugin.nexus.skip
+if [ "NO_DEPLOY" = "true" ]; then
+  if [ "$BRANCH" = "develop" ]; then
+    mvn -V -s ../build-resources/settings.xml org.jacoco:jacoco-maven-plugin:prepare-agent clean package sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=$SONAR_ORGANIZATION -Dsonar.login=$SONAR
+  elif [ "$BRANCH" = "master" ]; then
+    mvn -V -s ../build-resources/settings.xml clean package
+  else
+    mvn -V -s ../build-resources/settings.xml clean package
+  fi
 else
-  mvn -V -s ../build-resources/settings.xml clean package
+  if [ "$BRANCH" = "develop" ]; then
+    openssl aes-256-cbc -pass pass:${OPENSSL_PWD} -in ../build-resources/private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s ../build-resources/settings.xml org.jacoco:jacoco-maven-plugin:prepare-agent clean deploy sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=${SONAR_ORGANIZATION} -Dsonar.login=${SONAR}
+  elif [ "$BRANCH" = "master" ]; then
+    openssl aes-256-cbc -pass pass:${OPENSSL_PWD} -in ../build-resources/private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s ../build-resources/settings.xml clean deploy
+    mvn -V -s ../build-resources/settings.xml deploy -Dmaven.plugin.nexus.skip
+  else
+    mvn -V -s ../build-resources/settings.xml clean package
+  fi
 fi
