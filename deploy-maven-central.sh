4d3e2ab373720134b216b892b9f214b1418fb1c6
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-07-03 07:06:47 +0200

    [WIP] Support build parameters.

--- deploy-maven-central.sh
+++ deploy-maven-central.sh
@@ -19,19 +19,19 @@ echo "Building $BRANCH branch"
 
 if [ "$NO_DEPLOY" = "true" ]; then
   if [ "$BRANCH" = "develop" ]; then
-    mvn -V -s ../build-resources/settings.xml org.jacoco:jacoco-maven-plugin:prepare-agent clean package sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=$SONAR_ORGANIZATION -Dsonar.login=$SONAR
+    mvn -V -s ../build-resources/settings.xml org.jacoco:jacoco-maven-plugin:prepare-agent clean package sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=$SONAR_ORGANIZATION -Dsonar.login=$SONAR $MVN_PARAMS
   elif [ "$BRANCH" = "master" ]; then
-    mvn -V -s ../build-resources/settings.xml clean package
+    mvn -V -s ../build-resources/settings.xml clean package $MVN_PARAMS
   else
-    mvn -V -s ../build-resources/settings.xml clean package
+    mvn -V -s ../build-resources/settings.xml clean package $MVN_PARAMS
   fi
 else
   if [ "$BRANCH" = "develop" ]; then
-    openssl aes-256-cbc -pass pass:${OPENSSL_PWD} -in ../build-resources/private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s ../build-resources/settings.xml org.jacoco:jacoco-maven-plugin:prepare-agent clean deploy sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=${SONAR_ORGANIZATION} -Dsonar.login=${SONAR}
+    openssl aes-256-cbc -pass pass:${OPENSSL_PWD} -in ../build-resources/private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s ../build-resources/settings.xml org.jacoco:jacoco-maven-plugin:prepare-agent clean deploy sonar:sonar $MVN_PARAMS -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=${SONAR_ORGANIZATION} -Dsonar.login=${SONAR}
   elif [ "$BRANCH" = "master" ]; then
-    openssl aes-256-cbc -pass pass:${OPENSSL_PWD} -in ../build-resources/private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s ../build-resources/settings.xml clean deploy
-    mvn -V -s ../build-resources/settings.xml deploy -Dmaven.plugin.nexus.skip
+    openssl aes-256-cbc -pass pass:${OPENSSL_PWD} -in ../build-resources/private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s ../build-resources/settings.xml clean deploy $MVN_PARAMS
+    mvn -V -s ../build-resources/settings.xml deploy $MVN_PARAMS -Dmaven.plugin.nexus.skip
   else
-    mvn -V -s ../build-resources/settings.xml clean package
+    mvn -V -s ../build-resources/settings.xml clean package $MVN_PARAMS
   fi
 fi
