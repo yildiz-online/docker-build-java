c2bd3db2ee4ea1a0bb6deeb24896e1640f39492b
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2025-09-18 20:46:12 +0200

    [FEATURE] No longer rely on physical key.

--- deploy-maven-central.sh
+++ deploy-maven-central.sh
@@ -15,13 +15,10 @@ SECRETS=$(curl -sS -H "X-Vault-Token: $TOKEN" -X GET https://vault.yildiz-games.
 #Some variables need to be exported as env variable to be used by external processes.
 
 export GH_TOKEN=$(echo ${SECRETS} | jq -r '.data.GH_TOKEN')
-OPENSSL_PWD=$(echo ${SECRETS} | jq -r '.data.OPENSSL_PWD')
-export GPG_KEY=$(echo ${SECRETS} | jq -r '.data.GPG_KEY')
-export GPG_PWD=$(echo ${SECRETS} | jq -r '.data.GPG_PWD')
+export SIGN_KEY=$(echo ${SECRETS} | jq -r '.data.OSSRH_GPG_KEY')
+export SIGN_KEY_PASS=$(echo ${SECRETS} | jq -r '.data.OSSRH_GPG_PWD')
 export OSSRH_USER_TOKEN=$(echo ${SECRETS} | jq -r '.data.OSSRH_USER_TOKEN')
 export OSSRH_PWD_TOKEN=$(echo ${SECRETS} | jq -r '.data.OSSRH_PWD_TOKEN')
-export REPO_USER=$(echo ${SECRETS} | jq -r '.data.REPO_USER')
-export REPO_PASSWORD=$(echo ${SECRETS} | jq -r '.data.REPO_PASSWORD')
 SONAR=$(echo ${SECRETS} | jq -r '.data.SONAR')
 SONAR_ORGANIZATION=$(echo ${SECRETS} | jq -r '.data.SONAR_ORGANIZATION')
 
