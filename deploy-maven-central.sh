cb4c7d37cc52be1284a92e411fcc22444d5bb502
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2025-09-18 21:17:29 +0200

    [FEATURE] No longer rely on physical key.

--- deploy-maven-central.sh
+++ deploy-maven-central.sh
@@ -38,9 +38,9 @@ if [ "$NO_DEPLOY" = "true" ]; then
   fi
 else
   if [ "$BRANCH" = "develop" ]; then
-    openssl aes-256-cbc -pass pass:${OPENSSL_PWD} -in ../build-resources/private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s ../build-resources/settings.xml org.jacoco:jacoco-maven-plugin:prepare-agent clean deploy sonar:sonar $MVN_PARAMS -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=${SONAR_ORGANIZATION} -Dsonar.login=${SONAR}
+    mvn -V -s ../build-resources/settings.xml org.jacoco:jacoco-maven-plugin:prepare-agent clean deploy sonar:sonar $MVN_PARAMS -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=${SONAR_ORGANIZATION} -Dsonar.login=${SONAR}
   elif [ "$BRANCH" = "master" ]; then
-    openssl aes-256-cbc -pass pass:${OPENSSL_PWD} -in ../build-resources/private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -U -s ../build-resources/settings.xml clean deploy $MVN_PARAMS
+    mvn -V -U -s ../build-resources/settings.xml clean deploy $MVN_PARAMS
   else
     mvn -V -s ../build-resources/settings.xml clean package $MVN_PARAMS
   fi
